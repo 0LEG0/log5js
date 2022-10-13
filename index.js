7c9cccf4199e15f91d680f779c6a78d33dcee796
Anton <58663761+0LEG0@users.noreply.github.com>
2022-10-13 10:54:53 +0300

    Add code description

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-module.exports = {
+/**
+ * log5js
+ * @author "0LEG0 <a.i.s@gmx.com>"
+ * @version 1.0.0
+ * @license Apache-2.0
+ */
+"use strict";
+
+const LOG = {
     set level(lvl) {
         if (typeof lvl === "number") {
-            this._level = lvl;
+            LOG._level = lvl;
             return;
         }
         if (typeof lvl === "string") {
             switch(lvl.toUpperCase()) {
-                case "ERROR": this._level = 0; break;
-                case "WARN": this._level = 1; break;
-                case "NOTE": this._level = 2; break;
-                case "INFO": this._level = 3; break;
-                case "DEBUG": this._level = 4; break;
+                case "ERROR": LOG._level = 0; break;
+                case "WARN": LOG._level = 1; break;
+                case "NOTE": LOG._level = 2; break;
+                case "INFO": LOG._level = 3; break;
+                case "DEBUG": LOG._level = 4; break;
                 default:
             }
         }
     },
     get level() {
-        return this._level;
+        return LOG._level;
     },
     _level: 3,
     /* 0 - error, 1 - warn, 2 - note, 3 - info, 4 - debug */
-    error: function(...args) {this.log("ERROR", ...args)}, // 0
+    error: function(...args) {LOG.log("ERROR", ...args)}, // 0
     warn: function(...args) {
-        if (this._level > 0) this.log("WARN ", ...args)
+        if (LOG._level > 0) LOG.log("WARN ", ...args)
     },
     note: function(...args) {
-        if (this._level > 1) this.log("NOTE ", ...args)
+        if (LOG._level > 1) LOG.log("NOTE ", ...args)
     },
     info: function(...args) {
-        if (this._level > 2) this.log("INFO ", ...args)
+        if (LOG._level > 2) LOG.log("INFO ", ...args)
     },
     debug: function(...args) {
-        if (this._level > 3) this.log("DEBUG", ...args)
+        if (LOG._level > 3) LOG.log("DEBUG", ...args)
     },
     log: function(flag, ...args) {
         console.log(`${new Date().toISOString()} [${flag}]`, ...args);
     }
 }
+
+module.exports = LOG;
